124d7b6d34fab0b322bfaadcae2adc98d4a2fb98
Michael Rausch <me@michaelrausch.nz>
2021-12-12 22:33:41 +1300

    Added CORS middleware

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/michaelrausch/Contact-Form-Service/lib"
 	"github.com/michaelrausch/Contact-Form-Service/routes"
 )
@@ -40,6 +41,11 @@ func main() {
 	go sendSlackServerUpdate(*config)
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowOrigins: "*",
+	}))
 
 	app.Post("/", routes.ContactMessage)
 
